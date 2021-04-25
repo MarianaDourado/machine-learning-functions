428bab23780cc3d66ee7c84a06a8d4d5d1521dbc
Mariana <30739212+MarianaDourado@users.noreply.github.com>
2021-04-24 23:26:31 -0300

    Update softmax-function.py

--- softmax-function.py
+++ softmax-function.py
@@ -3,6 +3,9 @@ import numpy as np
 # Write a function that takes as input a list of numbers, and returns
 # the list of values given by the softmax function.
 def softmax(L):
+    """
+    Softmax function transforms input values into values between 0 and 1, so that they can be interpreted as probabilities.
+    """
     expL = np.exp(L) #"np.exp" - Calculate the exponential of all elements in the input array.
     result = []
     for i in expL:
